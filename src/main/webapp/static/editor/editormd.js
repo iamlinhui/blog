512573874bec0a75a0931c4691d926ccd78077f4
lynn <lynn@promptness.cn>
2021-10-31 14:53:08 +0800

    simple

--- src/main/webapp/static/editor/editormd.js
+++ src/main/webapp/static/editor/editormd.js
@@ -64,14 +64,22 @@
     editormd.classPrefix  = "editormd-";
     
     editormd.toolbarModes = {
+        // full : [
+        //     "undo", "redo", "|",
+        //     "bold", "del", "italic", "quote", "ucwords", "uppercase", "lowercase", "|",
+        //     "h1", "h2", "h3", "h4", "h5", "h6", "|",
+        //     "list-ul", "list-ol", "hr", "|",
+        //     "link", "reference-link", "image", "code", "preformatted-text", "code-block", "table", "datetime", "emoji", "html-entities", "pagebreak", "|",
+        //     "goto-line", "watch", "preview", "fullscreen", "clear", "search", "|",
+        //     "help", "info"
+        // ],
         full : [
-            "undo", "redo", "|", 
-            "bold", "del", "italic", "quote", "ucwords", "uppercase", "lowercase", "|", 
-            "h1", "h2", "h3", "h4", "h5", "h6", "|", 
+            "undo", "redo", "|",
+            "bold", "del", "italic", "quote", "ucwords", "uppercase", "lowercase", "|",
+            "h1", "h2", "h3", "h4", "h5", "h6", "|",
             "list-ul", "list-ol", "hr", "|",
             "link", "reference-link", "image", "code", "preformatted-text", "code-block", "table", "datetime", "emoji", "html-entities", "pagebreak", "|",
-            "goto-line", "watch", "preview", "fullscreen", "clear", "search", "|",
-            "help", "info"
+            "goto-line", "watch", "preview"
         ],
         simple : [
             "undo", "redo", "|", 
