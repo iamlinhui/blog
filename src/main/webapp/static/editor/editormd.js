55d8c24550c8d4e1f0c3b37865e6c3a970fe02a9
lynn <lynn@promptness.cn>
2021-10-31 15:09:37 +0800

    simple

--- src/main/webapp/static/editor/editormd.js
+++ src/main/webapp/static/editor/editormd.js
@@ -78,8 +78,8 @@
             "bold", "del", "italic", "quote", "ucwords", "uppercase", "lowercase", "|",
             "h1", "h2", "h3", "h4", "h5", "h6", "|",
             "list-ul", "list-ol", "hr", "|",
-            "link", "reference-link", "image", "code", "preformatted-text", "code-block", "table", "datetime", "emoji", "html-entities", "pagebreak", "|",
-            "goto-line", "watch", "preview"
+            "link", "reference-link", "image", "code", "preformatted-text", "code-block", "table", "datetime", "html-entities", "pagebreak", "|",
+            "goto-line", "watch", "preview", "search"
         ],
         simple : [
             "undo", "redo", "|", 
